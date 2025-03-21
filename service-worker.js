d3a63c32ff57f1048bc46b18294de0a9f2766f9f
Alireza Aghajani <60713538+aaghajani@users.noreply.github.com>
2025-03-21 04:26:42 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,40 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = "compass-app-v1";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./compass.png",
+    "./manifest.json",
+    "./service-worker.js"
+];
+
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open('compass-app').then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './icon.png'
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
         })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
     event.respondWith(
         caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+            return response || fetch(event.request).then((fetchResponse) => {
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        }).catch(() => caches.match("./index.html"))
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
         })
     );
 });
